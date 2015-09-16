76835d773d0efa45ef2055e8903bd8aa7efb9ab4
Mohanson <mohanson@163.com>
2015-09-16 17:12:18 +0800

    god knows

---
+++ README.md
@@ -0,0 +1,11 @@
+## Forward Manage
+
+## How Run
+    
+    python main.py
+    
+Then open browser, input `http://host:port/index.html`
+    
+## Thanks
+    
+    God
--- resources/templates.bac2/app/audit/audit.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Mohanson on 2015/7/11.
- */
-app.controller("audit", ['$scope', '$http', '$location', 'global', function ($scope, $http, $location, global) {
-        $scope.name = '111';
-    }]
-);
