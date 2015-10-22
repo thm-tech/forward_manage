c2f37dbea28c65297be4c03b7e9ba935fd1681ef
Mohanson <mohanson@163.com>
2015-10-22 14:56:07 +0800

    ii

--- src/dao/mongo_geo.py
+++ src/dao/mongo_geo.py
@@ -1,8 +1,12 @@
 # -*- coding:utf8 - *-
 
 import src.pydev.mongo
+from src.pydev.logger import create
+
+log_mongo = create('log_mongo')
 
 places = src.pydev.mongo.places
+log_mongo.info('connected mongodb success')
 
 
 def remove_shop(shop_id):
@@ -11,8 +15,12 @@ def remove_shop(shop_id):
 
 
 def insert_shop(shop_id, category_list, city_id, longitude, latitude):
-    return places.insert({'shop_id': int(shop_id),
-                          'category_list': category_list,
-                          'city_id': int(city_id),
-                          'local': [float(longitude), float(latitude)]
-                          })
+    response = places.insert({'shop_id': int(shop_id),
+                              'category_list': category_list,
+                              'city_id': int(city_id),
+                              'local': [float(longitude), float(latitude)]
+                              })
+    log_mongo.info('mongodb insert' + str(response))
+    return response
+
+insert_shop(10128, [206], 1048577, 117.286418, 31.837844)
